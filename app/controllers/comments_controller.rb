6d4b502843e1b15c5f2e67c97f87c0e1cf6fef25
Mike Goren <mgoren@mortalwombat.net>
2015-03-20 13:51:00 -0700

    add comments creation and list on post page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,20 @@
 class CommentsController < ApplicationController
 
-  
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment submitted!"
+    else
+      flash[:error] = "Comment not submitted!"
+    end
+    redirect_to post_path(@post) 
+  end
+
+
+private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 
 end
