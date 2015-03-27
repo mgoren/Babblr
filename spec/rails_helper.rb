cd6f4587f7f196f731e80141067dffb549c8d6c5
Mike Goren <mgoren@mortalwombat.net>
2015-03-27 14:24:13 -0700

    clean up rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -20,7 +18,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
